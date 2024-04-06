42f28368d9f8243ec31115878ea022558686e1a7
kubabartecki <169511@stud.prz.edu.pl>
2024-04-06 18:37:08 +0200

    add navigation

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,21 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import Test from './src/test/Test';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Home from './src/pages/home/Home';
+
+/* Theme variables */
+import "./src/theme/variables.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Test name={"Hackcarpathia"}/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Home />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
